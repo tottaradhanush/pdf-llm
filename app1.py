ddffbf5a7dec2b3af915adec04dbbd2b0d7697f8
Shubham Jadhav <85118952+20SHUBHAM@users.noreply.github.com>
2025-01-14 21:12:58 +0530

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,92 @@
+#AI Chatbot Assignment- Quicksell (AI Engineer)
+
+#Name: Shubham Jadhav (IIT ROORKEE)
+#Enrolment Number: 20410013
+
+
+import streamlit as st
+import os
+from langchain_groq import ChatGroq
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFLoader
+from dotenv import load_dotenv
+import tempfile
+
+load_dotenv()
+
+groq_api_key = os.getenv('GROQ_API_KEY')
+
+st.markdown("<h2 style='text-align: center;'>AI Chatbot Assignment-Quicksell:By Shubham Jadhav(IIT ROORKEE ,20410013) </h2>", unsafe_allow_html=True)
+
+llm = ChatGroq(groq_api_key=groq_api_key, model_name="Llama3-8b-8192")
+
+prompt = ChatPromptTemplate.from_template(
+    """
+    Answer the questions based on the provided context only.
+    Please provide the most accurate response based on the question.
+    <context>
+    {context}
+    <context>
+    Questions: {input}
+    """
+)
+
+def is_out_of_context(answer):
+    # Define criteria for determining if the answer is out of context
+    out_of_context_keywords = ["I don’t know", "not sure", "out of context", "invalid","There is no mention"," no mention"]
+    
+    # Check if any keyword is in the answer
+    for keyword in out_of_context_keywords:
+        if keyword in answer.lower():
+            return True
+    
+    return False
+
+def create_vector_db_out_of_the_uploaded_pdf_file(pdf_file):
+    if "vector_store" not in st.session_state:
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_file.write(pdf_file.read())
+            pdf_file_path = temp_file.name
+
+        st.session_state.embeddings = HuggingFaceEmbeddings(model_name='BAAI/bge-small-en-v1.5', model_kwargs={'device': 'cpu'}, encode_kwargs={'normalize_embeddings': True})
+        
+        st.session_state.loader = PyPDFLoader(pdf_file_path)
+        st.session_state.text_document_from_pdf = st.session_state.loader.load()
+        st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        
+        st.session_state.final_document_chunks = st.session_state.text_splitter.split_documents(st.session_state.text_document_from_pdf)
+        st.session_state.vector_store = FAISS.from_documents(st.session_state.final_document_chunks, st.session_state.embeddings)
+
+pdf_input_from_user = st.file_uploader("Please Upload your PDF file", type=['pdf'])
+
+if pdf_input_from_user is not None:
+    if st.button("Create your vector database"):
+        create_vector_db_out_of_the_uploaded_pdf_file(pdf_input_from_user)
+        st.success("PDF file is ready and we are good to go!")
+
+if "vector_store" in st.session_state:
+    user_prompt = st.text_input("Enter Your Question for uploaded PDF")
+
+    if st.button('Submit Prompt'):
+        if user_prompt:
+            if "vector_store" in st.session_state:
+                document_chain = create_stuff_documents_chain(llm, prompt)
+                retriever = st.session_state.vector_store.as_retriever()
+                retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+                response = retrieval_chain.invoke({'input': user_prompt})
+
+                # Check if the response is relevant or not
+                if is_out_of_context(response['answer']):
+                    st.write("Sorry, I didn’t understand your question. Do you want to connect with a live agent?")
+                else:
+                    st.write(response['answer'])
+            else:
+                st.write("Please upload the PDF first and create embeddings")
+        else:
+            st.error('Please write your prompt')
